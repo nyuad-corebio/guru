cc99de4bdc0ae8b2bec221af1ceec1e529e1c23a
NYUAD Core Bio <nyu10062@nyu.edu>
2023-04-11 12:23:44 +0400

    initial commit

--- scripts/airflow_conn.sh
+++ scripts/airflow_conn.sh
@@ -1,40 +1,30 @@
 #!/usr/bin/env bash
 
-#################################################################
+#############################################################################
 # Arguments for Airflow initialization
-# So far there is only the SSH / MYSQL / SMTP connection to Jubail
-#################################################################
+# So far these protocols SSH / MYSQL / SMTP needed now to connect to cluster
+############################################################################
 
 ## Defining SSH connection 
-#airflow connections add  airflow_docker_ssh   --conn-type ssh \
-#    --conn-host cgsb-build.abudhabi.nyu.edu \
-#    --conn-login jr5241 \
-#    --conn-port 4410 \
-#    --conn-extra '{"key_file": "/home/airflow/.ssh/docker_rsa", "missing_host_key_policy": "AutoAddPolicy"}'
-
-## Define jubail access
-airflow connections add  airflow_docker_ssh   --conn-type ssh \
-    --conn-host jubail.abudhabi.nyu.edu \
-    --conn-login gencore \
+airflow connections add  <ssh-conn-name>   --conn-type ssh \
+    --conn-host < hostname or IP address > \
+    --conn-login user \
     --conn-port 22 \
     --conn-extra '{"key_file": "/home/airflow/.ssh/id_rsa", "missing_host_key_policy": "AutoAddPolicy"}'
 
 ## Defining SMTP connection 
+airflow connections add <email-conn-name> --conn-type email \
+    --conn-host smtp.gmail.com \ 
+    --conn-login <emailID> \ 
+    --conn-password <email-pass> \ 
+    --conn-port <port-num>
 
-#airflow connections add airflow_docker_email --conn-type email \
-#    --conn-host smtp.gmail.com \ 
-#    --conn-login nyu10062@nyu.edu \ 
-#    --conn-password jicbsgkxpnbtgdxa \ 
-#    --conn-port 465
-
-airflow connections add airflow_docker_email --conn-type email --conn-host smtp.gmail.com --conn-login nyu10062@nyu.edu --conn-password jicbsgkxpnbtgdxa --conn-port 465
 ## Defining Mysql Connection 
-#airflow connections add airflow_docker_mysql --conn-type mysql \ 
-#    --conn-login root \
-#    --conn-password test \
-#    --conn-host cgsb-build.abudhabi.nyu.edu \
-#    --conn-port 13307 \
-#    --conn-schema lims \
-#    --conn-extra '{"ssl_mode": "DISABLED"}'
+airflow connections add <mysql-conn-name> --conn-type mysql \ 
+    --conn-login <user> \
+    --conn-password <pass> \
+    --conn-host <hostname or IP address > \
+    --conn-port <port> \
+    --conn-schema <database name> \
+    --conn-extra '{"ssl_mode": "DISABLED"}'
 
-airflow connections add airflow_docker_mysql --conn-type mysql --conn-login root --conn-password test --conn-host cgsb-build.abudhabi.nyu.edu --conn-port 13307 --conn-schema lims --conn-extra '{"ssl_mode": "DISABLED"}'
